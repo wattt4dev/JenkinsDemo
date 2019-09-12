324214df4f74da4245b64116f04e982f5ec541a4
wattt4dev <54113275+wattt4dev@users.noreply.github.com>
2019-09-12 16:08:49 +0200

    Update DemoApplication.java

--- demo/src/main/java/fr/adaming/demo/DemoApplication.java
+++ demo/src/main/java/fr/adaming/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package fr.adaming.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
